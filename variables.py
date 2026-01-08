1e79789ff30c3d762223c807072bfb0fd549648b
RohanNale <rohansnale143@email.com>
2026-01-08 13:18:39 +0530

    Add variables.py - python basics

---
+++ variables.py
@@ -0,0 +1,22 @@
+a = complex(9, 3)
+b = True
+c = "Rohan"
+d = None
+print(a)
+print(b)
+
+print("The types of a is ", type(a))
+
+print("The types of b is ", type(b))
+
+print("The types of c is ", type(c))
+
+
+list1 = [9,8,6,1,4,[-9,6],["apple","banana"]]
+print(list1)
+
+tuple1 = (("parrot","sparrow"),("lion","tiger"))
+print(tuple1)
+
+dict1 ={ "name":"Rohan", "age":20, "conVote":True}
+print(dict1)
